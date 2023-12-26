cfd19e21e720aba6f3cedbe2680fa7f96957a227
abdirrahman <abdirrahmang@gmail.com>
2023-12-26 20:47:19 +0000

    Complete Day 3

--- Rust/2023/day-03/src/bin/part2.rs
+++ Rust/2023/day-03/src/bin/part2.rs
@@ -5,31 +5,39 @@ use std::io::BufReader;
 
 fn main() -> std::io::Result<()> {
     let f = File::open("./part1.txt")?;
-    let coords = array_2d(f);
+    let coords: Vec<((usize, usize), char)> = array_2d(f);
     let _numbers: HashMap<(usize, usize), char> = HashMap::new();
     let mut symbols: HashMap<(usize, usize), char> = HashMap::new();
+    let mut gears: HashMap<(usize, usize), char> = HashMap::new();
     for line in &coords {
         if !line.1.is_digit(10) && line.1 != '.' {
             symbols.insert((line.0 .0, line.0 .1), line.1);
         }
     }
 
+    for line in &coords {
+        if line.1 == '*' {
+            gears.insert((line.0 .0, line.0 .1), line.1);
+        }
+    }
+
     let mut tbs: Vec<i32> = Vec::new();
+    let mut store_coords: Vec<(Vec<(usize, usize)>, i32)> = Vec::new();
     let mut store: Vec<(usize, usize)> = Vec::new();
     let mut store_digits = Vec::new();
     for line in coords {
         for items in store.clone() {
             if items.0 == 139 {
-                println!(" 139 {:?}", store);
-                if check_symbol(store.clone(), symbols.clone()) {
-                    tbs.push(
+                if check_symbol(store.clone(), gears.clone()) {
+                    store_coords.push((
+                        store.clone(),
                         store_digits
                             .clone()
                             .into_iter()
                             .collect::<String>()
                             .parse::<i32>()
                             .unwrap(),
-                    );
+                    ));
                     store.clear();
                     store_digits.clear();
                 }
@@ -43,14 +51,15 @@ fn main() -> std::io::Result<()> {
             store_digits.push(line.1)
         } else if !store.is_empty() {
             if check_symbol(store.clone(), symbols.clone()) {
-                tbs.push(
+                store_coords.push((
+                    store.clone(),
                     store_digits
                         .clone()
                         .into_iter()
                         .collect::<String>()
                         .parse::<i32>()
                         .unwrap(),
-                );
+                ));
                 store.clear();
                 store_digits.clear();
             }
@@ -58,9 +67,7 @@ fn main() -> std::io::Result<()> {
             store_digits.clear();
         }
     }
-
-    println!("{:?}", tbs.iter().sum::<i32>());
-
+    count_gears(store_coords, gears);
     Ok(())
 }
 
@@ -78,6 +85,67 @@ fn array_2d(input: File) -> Vec<((usize, usize), char)> {
         }
     }
     return grid;
+}
+
+fn count_gears(
+    digits_coords: Vec<(Vec<(usize, usize)>, i32)>,
+    gears: HashMap<(usize, usize), char>,
+) {
+    let mut gear_pair: Vec<i32> = Vec::new();
+    let mut sum_pairs: Vec<i32> = Vec::new();
+    for (key, _value) in gears.into_iter() {
+        for i in digits_coords.clone().into_iter() {
+            if check_gear(i.0, key) {
+                gear_pair.push(i.1);
+            }
+        }
+
+        if gear_pair.len() == 2 {
+            sum_pairs.push(gear_pair.iter().product());
+            gear_pair.clear()
+        }
+        gear_pair.clear()
+    }
+    println!("Sum of gear pairs: {:?}", sum_pairs.iter().sum::<i32>())
+}
+
+fn check_gear(vec: Vec<(usize, usize)>, key: (usize, usize)) -> bool {
+    for line in vec {
+        let x_axis: [(usize, usize); 2] = [
+            (line.0.checked_sub(1).unwrap_or(10000), line.1),
+            (line.0 + 1, line.1),
+        ];
+        let up_yaxis: [(usize, usize); 3] = [
+            (line.0, line.1.checked_sub(1).unwrap_or(10000)),
+            (
+                line.0.checked_sub(1).unwrap_or(10000),
+                line.1.checked_sub(1).unwrap_or(10000),
+            ),
+            (line.0 + 1, line.1.checked_sub(1).unwrap_or(10000)),
+        ];
+        let down_yaxis: [(usize, usize); 3] = [
+            (line.0, line.1 + 1),
+            (line.0.checked_sub(1).unwrap_or(10000), line.1 + 1),
+            (line.0 + 1, line.1 + 1),
+        ];
+
+        for co in x_axis {
+            if co == key {
+                return true;
+            }
+        }
+        for co in up_yaxis {
+            if co == key {
+                return true;
+            }
+        }
+        for co in down_yaxis {
+            if co == key {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 fn check_symbol(store: Vec<(usize, usize)>, symbol: HashMap<(usize, usize), char>) -> bool {
